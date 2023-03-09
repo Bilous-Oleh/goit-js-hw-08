ed7ac8d37546ea045ad6433ab49b36ab19bca47d
Bilous-Oleh <2005net2005@gmail.com>
2023-03-09 22:52:12 +0200

    Done task-01

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,32 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 // Add imports above this line
+
 import { galleryItems } from './gallery-items';
+
 // Change code below this line
+const makeImageGalleryCard = ({ description, original, preview }) =>
+  `<li>
+    <div class="gallery">
+        <a class="gallery__item" href=${original}>
+            <img class="gallery__image" src=${preview} alt="${description}"/>
+        </a>
+    </div>
+  </li>`;
+
+const galleryEl = document.querySelector('.gallery');
+
+const markup = galleryItems
+  .map(element => makeImageGalleryCard(element))
+  .join('');
+
+galleryEl.innerHTML = markup;
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  caption: true,
+  captionDelay: 250,
+  captionsData: 'alt',
+});
 
 console.log(galleryItems);
