61ed4caf75f5e1066f799cb925df4ff7cc8c8629
Bilous-Oleh <2005net2005@gmail.com>
2023-03-12 23:51:53 +0200

    Done task-03

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,36 @@
+import throttle from 'lodash.throttle';
 
+const LOCAL_STORAGE_KEY = 'feedback-form-state';
+
+const formEl = document.querySelector('.feedback-form');
+const feedbackForm = {};
+
+formEl.addEventListener('input', throttle(handleInputForm, 500));
+formEl.addEventListener('submit', handleSubmitForm);
+
+fillForm();
+
+function handleInputForm(event) {
+  event.preventDefault();
+
+  const { email, message } = event.currentTarget;
+
+  feedbackForm.email = email.value;
+  feedbackForm.message = message.value;
+  localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(feedbackForm));
+}
+
+function handleSubmitForm(event) {
+  event.preventDefault();
+  event.currentTarget.reset();
+  localStorage.removeItem(LOCAL_STORAGE_KEY);
+}
+
+function fillForm() {
+  const savedForm = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+
+  if (savedForm) {
+    formEl.email.value = savedForm.email;
+    formEl.message.value = savedForm.message;
+  }
+}
