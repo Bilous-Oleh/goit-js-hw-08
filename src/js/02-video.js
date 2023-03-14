9a9039b43e68b8e783db16839d4aabf4c093f243
Bilous-Oleh <2005net2005@gmail.com>
2023-03-14 22:59:59 +0200

    Fix bugs_1.1 task-02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,19 +1,18 @@
 import throttle from 'lodash.throttle';
 import Player from '@vimeo/player';
 
+const PLAYER_CURRENT_TIME = 'videoplayer-current-time';
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
+
 const timeUpdate = function (time) {
-  localStorage.setItem(
-    'videoplayer-current-time',
-    JSON.stringify(time.seconds)
-  );
+  localStorage.setItem(PLAYER_CURRENT_TIME, time.seconds);
 };
 
 player.on('timeupdate', throttle(timeUpdate, 1000));
 
-const savedPlayerTime = localStorage.getItem('videoplayer-current-time');
-const stopedPlayerTime = JSON.parse(savedPlayerTime);
+const savedPlayerTime = localStorage.getItem(PLAYER_CURRENT_TIME);
+const stopedPlayerTime = savedPlayerTime;
 
 currentTime();
 
